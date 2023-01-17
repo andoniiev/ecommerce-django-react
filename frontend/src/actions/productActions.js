4e547debbdc1dc2a7b95bb9b059b59a107e5beed
Bogdan Andoniiev <andonievvv@gmail.com>
2023-01-17 22:05:52 +0100

    uploaded frontend actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import {  useParams } from "react-router-dom";
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+} from "../constants/productConstants";
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products/");
+
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_DETAILS_REQUEST });
+      
+      const { data } = await axios.get(`/api/products/${id}`);
+  
+      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_DETAILS_FAIL,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+      });
+    }
+  };
+  
+
+  export const deleteProduct = (id) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_DELETE_REQUEST,
+      });
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+  
+      const { data } = await axios.delete(`/api/products/delete/${id}/`, config);
+  
+      dispatch({
+        type: PRODUCT_DELETE_SUCCESS,
+      });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_DELETE_FAIL,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+      });
+    }
+  };
