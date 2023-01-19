ae6abb40293ff8aab660b45ff0d091ff8f794aa3
Bogdan Andoniiev <andonievvv@gmail.com>
2023-01-19 23:13:55 +0100

    fixed the Product.js styling

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,36 +1,39 @@
-import React from 'react'
-import { Card } from 'react-bootstrap';
-import Rating from './Rating'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+import "../index.css";
 
-function Product({product}) {
+function Product({ product }) {
   return (
-    <Card className ="my-3 p-3 rounded">
-      <Link to ={`/product/${product._id}`}>
-        <Card.Img src = {product.image}/>
+    <Card style={{ height: "450px" }} className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} />
       </Link>
 
       <Card.Body>
-      <Link to ={`/product/${product._id}`}>
-        <Card.Title as="div">
+        <Link to={`/product/${product._id}`} className="title-link">
+          <Card.Title as="div">
             <strong>{product.name}</strong>
-        </Card.Title>
-      </Link>
+          </Card.Title>
+        </Link>
 
-      <Card.Text as="div">
-        <div className ="my-3">
-           
-            <Rating value = {product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
-        </div>
-      </Card.Text>
+        <Card.Text as="div">
+          <div className="my-3">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color={"#f8e825"}
+            />
+          </div>
+        </Card.Text>
 
-      <Card.Text as="h3">
-        ${product.price}
-      </Card.Text>
-
+        <Card.Text className="price" as="h3">
+          ${product.price}
+        </Card.Text>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default Product
+export default Product;
